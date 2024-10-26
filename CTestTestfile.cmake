5a706a361cd8492f178f5fa5fac72305179dd541
Stefan Goetschi <github@stefan-goetschi.ch>
2024-10-26 17:24:51 +0200

    Remove unused CTest definitions.

--- CTestTestfile.cmake
+++
@@ -1,4 +0,0 @@
-
-add_test(berlin_open_data_sample_data.citygml COMMAND bin/citygmltest ../data/berlin_open_data_sample_data.citygml)
-add_test(b1_lod2_s.gml COMMAND bin/citygmltest ../data/b1_lod2_s.gml)
-add_test(FZK-Haus-LoD0-KIT-IAI-KHH-B36-V1.gml COMMAND bin/citygmltest ../data/FZK-Haus-LoD0-KIT-IAI-KHH-B36-V1.gml)
