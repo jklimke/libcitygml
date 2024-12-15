23724259a92b8d0cb9ed2bdba7ef1873c9991ed8
Stefan Goetschi <github@stefan-goetschi.ch>
2024-12-15 13:36:53 +0100

    Adapt Tests to new CityObjectsTypeMask interface.
    This can be seen as a guide on how to transition existing
    client code to the new interface.
    Most probably there is no such code because the filtering
    doesn't work currently.

--- systemTests/CityObjectsTypeMaskTests.cpp
+++ systemTests/CityObjectsTypeMaskTests.cpp
@@ -21,7 +21,7 @@ using namespace citygml;
 
 TEST(CityObjectsTypeMask, FilterBuilding) {
     ParserParams params;
-    EXPECT_EQ(CityObjectsTypeMask(CityObject::CityObjectsType::COT_All), params.objectsMask);
+    EXPECT_EQ(toMask(CityObject::CityObjectsType::COT_All), params.objectsMask);
 
     params.objectsMask = CityObject::CityObjectsType::COT_All;
     readBridgeAndBuildingWithParams(params, 2);
@@ -39,80 +39,90 @@ TEST(CityObjectsTypeMask, FilterBuilding) {
     readBridgeAndBuildingWithParams(params, 1);
 }
 
-// TODO: Make masks const - prohibited by non-const equality operator.
 TEST(CityObjectsTypeMask, CityObjectTypeBitwiseOperations) {
     CityObjectsTypeMask const defaultMask;
-    EXPECT_EQ(0, defaultMask);
+    EXPECT_EQ(0, defaultMask.count());
 
-    CityObjectsTypeMask /*const*/ fullMask(CityObject::CityObjectsType::COT_All);
-    EXPECT_TRUE(fullMask == CityObject::CityObjectsType::COT_All);
+    CityObjectsTypeMask const fullMask(toMask(CityObject::CityObjectsType::COT_All));
+    EXPECT_TRUE(fullMask == toMask(CityObject::CityObjectsType::COT_All));
+    EXPECT_EQ(fullMask.count(), static_cast<size_t>(CityObject::CityObjectsType::COT_All));
 
-    CityObjectsTypeMask /*const*/ emptyMask(~CityObject::CityObjectsType::COT_All);
+    CityObjectsTypeMask const emptyMask(~CityObject::CityObjectsType::COT_All);
     EXPECT_EQ(emptyMask, 0);
 
-    CityObjectsTypeMask /*const*/ buildingMask(CityObject::CityObjectsType::COT_All & CityObject::CityObjectsType::COT_Building);
-    EXPECT_TRUE(buildingMask == CityObject::CityObjectsType::COT_Building);
+    CityObjectsTypeMask const buildingMask(CityObject::CityObjectsType::COT_All & CityObject::CityObjectsType::COT_Building);
+    EXPECT_TRUE(buildingMask == toMask(CityObject::CityObjectsType::COT_Building));
+    EXPECT_EQ(buildingMask.count(), 1);
+    EXPECT_TRUE(buildingMask.test(static_cast<size_t>(CityObject::CityObjectsType::COT_Building)));
 
-    CityObjectsTypeMask /*const*/ buildingDoorMask(CityObject::CityObjectsType::COT_Building | CityObject::CityObjectsType::COT_Door);
-    EXPECT_TRUE(buildingDoorMask == static_cast<CityObject::CityObjectsType>(34));
+    CityObjectsTypeMask const buildingDoorMask(CityObject::CityObjectsType::COT_Building | CityObject::CityObjectsType::COT_Door);
+    EXPECT_EQ(buildingDoorMask.count(), 2);
+    EXPECT_TRUE(buildingDoorMask.test(static_cast<size_t>(CityObject::CityObjectsType::COT_Building)));
+    EXPECT_TRUE(buildingDoorMask.test(static_cast<size_t>(CityObject::CityObjectsType::COT_Door)));
 
-    CityObjectsTypeMask /*const*/ notBuildingMask(CityObject::CityObjectsType::COT_All ^ CityObject::CityObjectsType::COT_Building);
+    CityObjectsTypeMask const notBuildingMask(CityObject::CityObjectsType::COT_All ^ CityObject::CityObjectsType::COT_Building);
     EXPECT_TRUE(notBuildingMask == ~CityObject::CityObjectsType::COT_Building);
 }
 
-// TODO: Make masks const - prohibited by non-const equality operator.
 TEST(CityObjectsTypeMask, CityObjectTypeMaskBitwiseOperations) {
     CityObjectsTypeMask const defaultMask;
-    EXPECT_EQ(0, defaultMask);
+    EXPECT_EQ(0, defaultMask.count());
 
-    CityObjectsTypeMask /*const*/ fullMask = CityObjectsTypeMask(CityObject::CityObjectsType::COT_All);
-    EXPECT_TRUE(fullMask == CityObject::CityObjectsType::COT_All);
+    CityObjectsTypeMask const fullMask = toMask(CityObject::CityObjectsType::COT_All);
+    EXPECT_TRUE(fullMask == toMask(CityObject::CityObjectsType::COT_All));
+    EXPECT_EQ(fullMask.count(), static_cast<size_t>(CityObject::CityObjectsType::COT_All));
 
-    CityObjectsTypeMask const emptyMask = ~CityObjectsTypeMask(CityObject::CityObjectsType::COT_All);
+    CityObjectsTypeMask const emptyMask = ~toMask(CityObject::CityObjectsType::COT_All);
     EXPECT_EQ(emptyMask, 0);
 
-    CityObjectsTypeMask /*const*/ buildingMask = CityObjectsTypeMask(CityObject::CityObjectsType::COT_All) & CityObjectsTypeMask(CityObject::CityObjectsType::COT_Building);
-    EXPECT_TRUE(buildingMask == CityObject::CityObjectsType::COT_Building);
+    CityObjectsTypeMask const buildingMask = toMask(CityObject::CityObjectsType::COT_All) & toMask(CityObject::CityObjectsType::COT_Building);
+    EXPECT_TRUE(buildingMask == toMask(CityObject::CityObjectsType::COT_Building));
+    EXPECT_EQ(buildingMask.count(), 1);
+    EXPECT_TRUE(buildingMask.test(static_cast<size_t>(CityObject::CityObjectsType::COT_Building)));
 
-    CityObjectsTypeMask /*const*/ buildingDoorMask = CityObjectsTypeMask(CityObject::CityObjectsType::COT_Building) | CityObjectsTypeMask(CityObject::CityObjectsType::COT_Door);
-    EXPECT_TRUE(buildingDoorMask == static_cast<CityObject::CityObjectsType>(34));
+    CityObjectsTypeMask const buildingDoorMask = toMask(CityObject::CityObjectsType::COT_Building) | toMask(CityObject::CityObjectsType::COT_Door);
+    EXPECT_EQ(buildingDoorMask.count(), 2);
+    EXPECT_TRUE(buildingDoorMask.test(static_cast<size_t>(CityObject::CityObjectsType::COT_Building)));
+    EXPECT_TRUE(buildingDoorMask.test(static_cast<size_t>(CityObject::CityObjectsType::COT_Door)));
 
-    CityObjectsTypeMask /*const*/ notBuildingMask = CityObjectsTypeMask(CityObject::CityObjectsType::COT_All) ^ CityObjectsTypeMask(CityObject::CityObjectsType::COT_Building);
+    CityObjectsTypeMask const notBuildingMask = toMask(CityObject::CityObjectsType::COT_All) ^ toMask(CityObject::CityObjectsType::COT_Building);
     EXPECT_TRUE(notBuildingMask == ~CityObject::CityObjectsType::COT_Building);
 
     CityObjectsTypeMask assignMask;
-    assignMask |= CityObject::CityObjectsType::COT_All;
-    EXPECT_TRUE(assignMask == CityObject::CityObjectsType::COT_All);
-    assignMask ^= CityObject::CityObjectsType::COT_Building;
+    assignMask |= toMask(CityObject::CityObjectsType::COT_All);
+    EXPECT_EQ(assignMask.count(), static_cast<size_t>(CityObject::CityObjectsType::COT_All));
+    assignMask ^= toMask(CityObject::CityObjectsType::COT_Building);
     EXPECT_TRUE(assignMask == ~CityObject::CityObjectsType::COT_Building);
-    assignMask &= CityObject::CityObjectsType::COT_Door;
-    EXPECT_TRUE(assignMask == CityObject::CityObjectsType::COT_Door);
+    assignMask &= toMask(CityObject::CityObjectsType::COT_Door);
+    EXPECT_TRUE(assignMask == toMask(CityObject::CityObjectsType::COT_Door));
 
     CityObjectsTypeMask mixMaskLeft;
     mixMaskLeft = mixMaskLeft | CityObject::CityObjectsType::COT_All;
-    EXPECT_TRUE(mixMaskLeft == CityObject::CityObjectsType::COT_All);
+    EXPECT_EQ(mixMaskLeft.count(), static_cast<size_t>(CityObject::CityObjectsType::COT_All));
     mixMaskLeft = mixMaskLeft ^ CityObject::CityObjectsType::COT_Building;
     EXPECT_TRUE(mixMaskLeft == ~CityObject::CityObjectsType::COT_Building);
     mixMaskLeft = mixMaskLeft & CityObject::CityObjectsType::COT_Door;
-    EXPECT_TRUE(mixMaskLeft == CityObject::CityObjectsType::COT_Door);
+    EXPECT_TRUE(mixMaskLeft == toMask(CityObject::CityObjectsType::COT_Door));
 
     CityObjectsTypeMask mixMaskRight;
     mixMaskRight = CityObject::CityObjectsType::COT_All | mixMaskRight;
-    EXPECT_TRUE(mixMaskRight == CityObject::CityObjectsType::COT_All);
+    EXPECT_EQ(mixMaskRight.count(), static_cast<size_t>(CityObject::CityObjectsType::COT_All));
     mixMaskRight = CityObject::CityObjectsType::COT_Building ^ mixMaskRight;
     EXPECT_TRUE(mixMaskRight == ~CityObject::CityObjectsType::COT_Building);
     mixMaskRight = CityObject::CityObjectsType::COT_Door & mixMaskLeft;
-    EXPECT_TRUE(mixMaskRight == CityObject::CityObjectsType::COT_Door);
+    EXPECT_TRUE(mixMaskRight == toMask(CityObject::CityObjectsType::COT_Door));
 }
 
 TEST(CityObjectsTypeMask, FilterStreamInterface) {
-    std::stringstream inStream("34", std::ios_base::in);
+    std::stringstream inStream("100010", std::ios_base::in);
     CityObjectsTypeMask inMask;
     inStream >> inMask;
-    EXPECT_TRUE(inMask == (CityObject::CityObjectsType::COT_Building | CityObject::CityObjectsType::COT_Door));
+    EXPECT_EQ(inMask.count(), 2);
+    EXPECT_TRUE(inMask.test(static_cast<size_t>(CityObject::CityObjectsType::COT_Building)));
+    EXPECT_TRUE(inMask.test(static_cast<size_t>(CityObject::CityObjectsType::COT_Door)));
 
     std::stringstream outStream(std::ios_base::out);
     CityObjectsTypeMask outMask(CityObject::CityObjectsType::COT_Building | CityObject::CityObjectsType::COT_Door);
     outStream << outMask;
-    EXPECT_TRUE(outStream.str() == "34");
+    EXPECT_EQ(outStream.str(), "000000000000000000000000000000000000000000000000100010");
 }
