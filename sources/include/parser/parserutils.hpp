40f5d57da899f841979483a5ba563d85fefffff4
Stefan Goetschi <github@stefan-goetschi.ch>
2025-09-07 22:09:06 +0200

    Use new method in parseMatrix.

--- sources/include/parser/parserutils.hpp
+++ sources/include/parser/parserutils.hpp
@@ -2,7 +2,6 @@
 
 #include <iterator>
 #include <memory>
-#include <sstream>
 #include <string>
 #include <string_view>
 #include <tuple>
@@ -16,30 +15,6 @@
 #include <citygml/vecs.hpp>
 
 namespace citygml {
-
-    inline TransformationMatrix parseMatrix( const std::string &s, std::shared_ptr<citygml::CityGMLLogger>& logger, const DocumentLocation& location)
-    {
-        std::stringstream ss;
-        ss << s;
-
-
-        double matrix[16] = { 1.0, 0.0, 0.0, 0.0,
-                              0.0, 1.0, 0.0, 0.0,
-                              0.0, 0.0, 1.0, 0.0,
-                              0.0, 0.0, 0.0, 1.0 };
-
-        for (size_t i = 0; i < 16; ++i)
-        {
-            if(ss.eof()) {
-                CITYGML_LOG_WARN(logger, "Matrix with 16 elements expected, got '" << i + 1 << "' at " << location << ". Matrix may be invalid.");
-                break;
-            }
-
-            ss >> matrix[i];
-        }
-
-        return TransformationMatrix(matrix);
-    }
 
     template <typename T, std::enable_if_t<std::is_fundamental_v<T>, bool> = true>
     char const* readNextValue(std::string_view view, T& target) {
@@ -85,6 +60,25 @@ namespace citygml {
             view = view.substr(std::distance(view.data(), end));
         }
         return vec;
+    }
+
+    inline TransformationMatrix parseMatrix( const std::string &s, std::shared_ptr<citygml::CityGMLLogger>& logger, const DocumentLocation& location)
+    {
+        std::string_view view(s);
+
+        double matrix[16] = { 1.0, 0.0, 0.0, 0.0,
+                              0.0, 1.0, 0.0, 0.0,
+                              0.0, 0.0, 1.0, 0.0,
+                              0.0, 0.0, 0.0, 1.0 };
+        for (size_t i = 0; i < 16; ++i){
+            if(view.empty()) {
+                CITYGML_LOG_WARN(logger, "Matrix with 16 elements expected, got '" << i + 1 << "' at " << location << ". Matrix may be invalid.");
+                break;
+            }
+            char const* next = readNextValue(view, matrix[i]);
+            view = view.substr(std::distance(view.data(), next));
+        }
+        return TransformationMatrix(matrix);
     }
 
     inline std::string parseReference(const std::string& reference, std::shared_ptr<citygml::CityGMLLogger>& logger, const DocumentLocation& location) {
