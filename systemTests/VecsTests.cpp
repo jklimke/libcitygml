30995626e385f7b948ac7240ae616cccdee85736
Stefan Goetschi <github@stefan-goetschi.ch>
2025-10-18 13:37:03 +0200

    Add test for parseMatrix.

--- systemTests/VecsTests.cpp
+++ systemTests/VecsTests.cpp
@@ -1,12 +1,16 @@
 #include <citygml/vecs.hpp>
+#include <citygml/transformmatrix.h>
+#include <parser/parserutils.hpp>
 
 #include "GlobalLocaleSwitcher.hpp"
 
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 #include <array>
 #include <cmath>
 #include <limits>
+#include <sstream>
 #include <stdexcept>
 #include <string_view>
 using namespace std::literals;
@@ -251,4 +255,83 @@ TEST(VecsTests, DifferentGlobalLocale) {
     EXPECT_THROW(double3.fromString("0,0 1,0 2,0"), std::runtime_error);
     EXPECT_THROW(float4.fromString("0,0 1,0 2,0 3,0"), std::runtime_error);
     EXPECT_THROW(double4.fromString("0,0 1,0 2,0 3,0"), std::runtime_error);
+}
+
+namespace {
+    std::string const FILE_NAME = "testFile.gml";
+    class TestDocumentLocation : public citygml::DocumentLocation {
+    public:
+        const std::string& getDocumentFileName() const override { return FILE_NAME; }
+        uint64_t getCurrentLine() const override { return 7; }
+        uint64_t getCurrentColumn() const override { return 42; }
+    };
+    
+    struct MatrixTestData {
+        std::string_view sourceString;
+        std::array<double, 16> expectedDoubles;
+
+        MatrixTestData(std::string_view s, std::array<double, 16> matcher)
+            : sourceString(s), expectedDoubles(matcher) {}
+    };
+
+    class MemoryLogger : public citygml::CityGMLLogger {
+    private:
+        mutable std::string allLogs;
+    public:
+        MemoryLogger(LOGLEVEL level = LOGLEVEL::LL_ERROR) : citygml::CityGMLLogger(level) {}
+        void log(LOGLEVEL level, const std::string& message, const char* file=nullptr, int line=-1) const override {
+            allLogs += "Level: " + std::to_string(static_cast<size_t>(level)) + ": " + (file != nullptr ? file : "") + (line != -1 ? ":" + std::to_string(line) : "") + ": " + message + "\n";
+        }
+        std::string getLogsAndClear() {
+            return { std::move(allLogs) };
+        }
+    };
+
+} // anonymous namespace
+
+TEST(VecsTests, Matrix) {
+    const std::array<MatrixTestData, 3> TEST_STRINGS{
+        MatrixTestData{"0 -1 1 +1  0. -1. 1. +1.  0.0 -1.0 1.0 +1.0  0.00 -0.1 0.1 +0.1"sv, { 0., -1., 1., 1.,  0., -1., 1., 1.,  0., -1., 1., 1.,  0., -0.1, 0.1, 0.1 } },
+        MatrixTestData{"0.0e0 -1.1e0 1.1e0 +1.1e0  0.0e10 -1.1e10 1.1e10 +1.1e10  0.0e-10 -1.1e-10 1.1e-10 +1.1e-10  0.0e+10 -1.1e+10 1.1e+10 +1.1e+10"sv, { 0., -1.1, 1.1, 1.1,  0., -1.1e10, 1.1e10, 1.1e10,  0., -1.1e-10, 1.1e-10, 1.1e-10,  0., -1.1e10, 1.1e10, 1.1e10 } },
+        MatrixTestData{"INF -INF +INF 0  0 1 0 0  0 0 1 0  0 0 0 1"sv, { doubleLimits::infinity(), -doubleLimits::infinity(), doubleLimits::infinity(), 0.,  0., 1., 0., 0.,  0., 0., 1., 0.,  0., 0., 0., 1. } },
+    };
+    std::shared_ptr<citygml::CityGMLLogger> noLogger;
+    TestDocumentLocation loc;
+
+    for (MatrixTestData const& data : TEST_STRINGS) {
+        citygml::TransformationMatrix matrix = parseMatrix(std::string(data.sourceString), noLogger, loc);
+        std::vector<double> matrixCopy(matrix.getMatrix(), matrix.getMatrix() + 16);
+        ASSERT_THAT(matrixCopy, testing::ElementsAreArray(data.expectedDoubles));
+    }
+
+    citygml::TransformationMatrix nanMatrix = parseMatrix(std::string("NaN -NaN +NaN 0  0 1 0 0  0 0 1 0  0 0 0 1"sv), noLogger, loc);
+    ASSERT_TRUE(std::isnan(nanMatrix.getMatrix()[0]));
+    ASSERT_TRUE(std::isnan(nanMatrix.getMatrix()[1]));
+    ASSERT_TRUE(std::isnan(nanMatrix.getMatrix()[2]));
+
+    constexpr std::array<std::string_view, 11> ERROR_STRINGS{
+        "0,0     0 0 0 0 0 0 0 0 0 0 0 0 0 0 0"sv,
+        "0,      0 0 0 0 0 0 0 0 0 0 0 0 0 0 0"sv,
+        ",0      0 0 0 0 0 0 0 0 0 0 0 0 0 0 0"sv,
+        "1.000,0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0"sv,
+        "1,000.0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0"sv,
+        "1'000.0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0"sv,
+        "0.0.0   0 0 0 0 0 0 0 0 0 0 0 0 0 0 0"sv,
+        ".0.0    0 0 0 0 0 0 0 0 0 0 0 0 0 0 0"sv,
+        "11e     0 0 0 0 0 0 0 0 0 0 0 0 0 0 0"sv,
+        "11e+    0 0 0 0 0 0 0 0 0 0 0 0 0 0 0"sv,
+        "11e-    0 0 0 0 0 0 0 0 0 0 0 0 0 0 0"sv,
+    };
+    for (std::string_view const& sourceString : ERROR_STRINGS) {
+        EXPECT_THROW(parseMatrix(std::string(sourceString), noLogger, loc), std::runtime_error);
+    }
+
+    auto logger = std::make_shared<MemoryLogger>(citygml::CityGMLLogger::LOGLEVEL::LL_WARNING);
+    auto citygmlLogger = std::static_pointer_cast<citygml::CityGMLLogger>(logger);
+    citygml::TransformationMatrix matrix = parseMatrix(std::string("0 0 0 1"sv), citygmlLogger, loc);
+    std::string const logs = logger->getLogsAndClear();
+    std::stringstream locatorString;
+    locatorString << loc;
+    EXPECT_THAT(logs, testing::StartsWith("Level: 3")); // Checks that log level is Warning
+    EXPECT_THAT(logs, testing::HasSubstr("Matrix with 16 elements expected, got '5' at " + locatorString.str() + ". Matrix may be invalid."));
 }
